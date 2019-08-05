b3cdfa05389cbcb4d6edf6fb6b8509c024c80dce
Ahmed AlMoselhy <ahmedalmoselhy.slm@gmail.com>
2019-08-05 17:49:10 +0200

    displaying error messages according to error location in form

--- register.php
+++ register.php
@@ -86,25 +86,61 @@ if(isset($_POST['reg_btn'])){
             echo $_SESSION['reg_fname'];
         } ?>" required>
         <br>
+        <?php
+        if(in_array("First name can't be more than 25 characters or less than 2 characters", $error_array)){
+            echo "First name can't be more than 25 characters or less than 2 characters<br>";
+        }
+        ?>
         <input type="text" name="reg_lname" placeholder="Last Name" value="<?php 
         if(isset($_SESSION['reg_lname'])){
             echo $_SESSION['reg_lname'];
         } ?>" required>
         <br>
+        <?php
+        if(in_array("Last name can't be more than 25 characters or less than 2 characters", $error_array)){
+            echo "Last name can't be more than 25 characters or less than 2 characters<br>";
+        }
+        ?>
         <input type="email" name="reg_email" placeholder="Email" value="<?php 
         if(isset($_SESSION['reg_email'])){
             echo $_SESSION['reg_email'];
         } ?>" required>
         <br>
+        <?php
+        if(in_array("Email already in use", $error_array)){
+            echo "Email already in use<br>";
+        }
+        if(in_array("Invalid Email Format", $error_array)){
+            echo "Invalid Email Format<br>";
+        }
+        ?>
         <input type="email" name="reg_email2" placeholder="Confirm Email" value="<?php 
         if(isset($_SESSION['reg_email2'])){
             echo $_SESSION['reg_email2'];
         } ?>" required>
         <br>
+        <?php
+        if(in_array("Emails don't match", $error_array)){
+            echo "Emails don't match<br>";
+        }
+        ?>
         <input type="password" name="reg_password" placeholder="Password" required>
         <br>
+        <?php
+        if(in_array("Your password can only contain english letters or numbers", $error_array)){
+            echo "Your password can only contain english letters or numbers<br>";
+        }
+        if(in_array("Your password must be between 5 and 30 characters", $error_array)){
+            echo "Your password must be between 5 and 30 characters<br>";
+        }
+        ?>
         <input type="password" name="reg_password2" placeholder="Confirm Password" required>
         <br>
+        <?php
+        if(in_array("Passwords don't match", $error_array)){
+            echo "Passwords don't match<br>";
+        }
+        ?>
         <input type="submit" name="reg_btn" value="Register">
     </form>
 </body>
