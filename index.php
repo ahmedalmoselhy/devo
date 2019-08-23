76915a9513db1e35602c8f2e4747e30bfc83315c
Ahmed AlMoselhy <ahmedalmoselhy.slm@gmail.com>
2019-08-23 17:35:25 +0200

    seperating user details to another file

--- index.php
+++ index.php
@@ -1,23 +1,7 @@
 <?php include "includes/header.php"; ?>
+    <?php include "user_details.php"; ?>
 
-
-    <div class="user_details column">
-        <a href="#">
-            <img src="<?php echo $user['profile_pic']; ?>" alt="">
-        </a>
-        <div class="user_details_left_right">
-            <a href="#">
-                <?php
-                echo $user['fname'] . " " . $user['lname'];
-                ?>
-            </a>
-            <br>
-            <?php
-            echo "Posts: " . $user['num_posts'] . "<br>";
-            echo "Likes: " . $user['num_likes'] . "<br>";
-            ?>
-        </div>
-    </div>
+    
 </div>
 
 </body>
