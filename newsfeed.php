b0c0960d3dae947e0a4b9d74422f6d5ab16f5062
Ahmed AlMoselhy <ahmedalmoselhy.slm@gmail.com>
2019-08-23 18:12:34 +0200

    styling the create post form

--- newsfeed.php
+++ newsfeed.php
@@ -1,7 +1,9 @@
 <div class="main_column column container">
     <form class="form_post" action="index.php" method="POST">
         <textarea name="post_text" id="post_text" cols="30" rows="3" placeholder="What's on your mind?" class="form-control"></textarea>
-        <input type="submit" name="post" id="post_btn" value="Post" class="btn btn-success">
+        <br>
+        <input type="submit" name="post" id="post_btn" value="Post" class="btn btn-success form-control">
+        <br>
         <hr>
     </form>
 </div>
